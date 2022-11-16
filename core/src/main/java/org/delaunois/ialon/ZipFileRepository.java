bf5608d4188938cbc6a4da630d5a275a42222c4d
Cédric de Launois <>
2022-11-16 21:17:00 +0100

    Added GPL header where needed

--- core/src/main/java/org/delaunois/ialon/ZipFileRepository.java
+++ core/src/main/java/org/delaunois/ialon/ZipFileRepository.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2022 Cédric de Launois
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.delaunois.ialon;
 
 import com.google.protobuf.ByteString;
@@ -8,13 +25,7 @@ import com.rvandoosselaer.blocks.BlocksConfig;
 import com.rvandoosselaer.blocks.Chunk;
 import com.rvandoosselaer.blocks.ChunkRepository;
 import com.simsilica.mathd.Vec3i;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
+
 import org.delaunois.ialon.protobuf.BlocksProtos;
 
 import java.io.FileOutputStream;
@@ -29,6 +40,14 @@ import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * A File repository implementation for loading and storing chunks using the Protocol Buffers method
--- core/src/main/java/org/delaunois/ialon/state/IalonDebugState.java
+++ core/src/main/java/org/delaunois/ialon/state/IalonDebugState.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2022 Cédric de Launois
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.delaunois.ialon.state;
 
 import com.jme3.app.Application;
@@ -28,10 +45,6 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
-/**
- * @author rvandoosselaer
- */
 public class IalonDebugState extends BaseAppState {
 
     private static final int MB = 1024 * 1024;
